7c5157259a72eaabcd6e21385470154006437fdb
chandragie <chandragie@gmail.com>
2023-02-10 17:13:29 +0700

    Example for BCAITWORKS 2023

---
+++ src/main/java/com/bca/adam/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.bca.adam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String index() {
+        return "Hello World!";
+    }
+
+    @GetMapping("hi")
+    public String hi() {
+        return "Hi World!";
+    }
+}
